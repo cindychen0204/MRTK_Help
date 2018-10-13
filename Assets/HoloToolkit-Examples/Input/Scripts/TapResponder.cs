1665d641da3216f7af2d8d4ca85c25b4c4641d2d
DESKTOP-UV1HCCJ\KinKinCindy <cindychen0204@gmail.com>
2018-10-13 10:07:25 +0900

    1012

--- Assets/HoloToolkit-Examples/Input/Scripts/TapResponder.cs
+++ Assets/HoloToolkit-Examples/Input/Scripts/TapResponder.cs
@@ -14,10 +14,10 @@ namespace HoloToolkit.Unity.InputModule.Tests
     {
         public void OnInputClicked(InputClickedEventData eventData)
         {
-            // Increase the scale of the object just as a response.
-            gameObject.transform.localScale += 0.05f * gameObject.transform.localScale;
+           // Increase the scale of the object just as a response.
+           // gameObject.transform.localScale += 0.05f * gameObject.transform.localScale;
 
-            eventData.Use(); // Mark the event as used, so it doesn't fall through to other handlers.
+           // eventData.Use(); // Mark the event as used, so it doesn't fall through to other handlers.
         }
     }
 }
